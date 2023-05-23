d8d306397e534150cdabb484e0b888c0bb9b9dfb
João Pedro Padilha <jppadilha11@gmail.com>
2023-05-23 16:51:28 -0300

    feats

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const s = (e) => document.querySelector(e);
 const sa = (e) => document.querySelectorAll(e);
 
+salgadoQT = 1;
+
 salgadosJSON.map((item, index) => {
     let salgadoItem = s('.models').cloneNode(true);
     
@@ -9,12 +11,39 @@ salgadosJSON.map((item, index) => {
     salgadoItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     salgadoItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
-        s('.salgado--area').style.display = 'flex';
+        s('.salgado--area-info span').innerHTML = item.name;
+        s('.salgado--area-img img').src = item.img;
+        s('.salgado--area-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+        salgadoQT = 1;
 
+        s('.salgado--area').style.opacity = 0;
+        s('.salgado--area').style.display = 'flex';
         setTimeout(() => {
             s('.salgado--area').style.opacity = 1;
         },200);
+
     });
 
     s('.section--area').append(salgadoItem)
+});
+
+function closeWindowArea() { 
+        setTimeout(() => {
+            s('.salgado--area').style.opacity = 0;
+        },200);
+        s('.salgado--area').style.display = 'none';
+}
+
+s('#menos').addEventListener('click', (e) => {
+    e.preventDefault();
+    if(salgadoQT > 1){
+        salgadoQT--;
+        s('.button-qt div').innerHTML = salgadoQT;
+    }
+});
+
+s('#mais').addEventListener('click', (e) => {
+    e.preventDefault();
+    salgadoQT++;
+    s('.button-qt div').innerHTML = salgadoQT;
 })
